49a3c05573447e2de385d252faadb1193abe5070
Jean-Paul <jpzambrano.leon@gmail.com>
2023-07-18 15:35:20 -0500

    modified file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,43 @@ export default function Navbar() {
     <header className="header" id="header">
       <nav className="nav container">
         <a href="#" className="nav__logo">
-          Holu
+          Holu <i className="bx bxs-home-alt-2"></i>
+        </a>
+
+        <div className="nav__menu">
+          <ul className="nav__menu__list">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="bx bx-home-alt-2"></i>
+                <span>Home</span>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#popular" className="nav__link">
+                <i className="bx bx-building-house"></i>
+                <span>Residences</span>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#value" className="nav__link">
+                <i className="bx bx-award"></i>
+                <span>Value</span>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="bx bx-phone"></i>
+                <span>Contact</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <a href="#" className="button nav__button">
+          Subscribe
         </a>
       </nav>
     </header>
