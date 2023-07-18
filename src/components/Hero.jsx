2eee78093f28c3586cb3327afac16699d415e228
Jean-Paul <jpzambrano.leon@gmail.com>
2023-07-18 18:21:09 -0500

    modified file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,56 +1,56 @@
 export default function Hero() {
   return (
-    <section className="hero section" id="hero">
-      <div className="hero__container container grid">
-        <div className="hero__data">
-          <h1 className="hero__title">
+    <section className="hero-section section" id="hero">
+      <div className="hero-container container grid">
+        <div className="hero-text">
+          <h1 className="hero-text__title">
             Discover <br /> Most Suitable <br /> Property
           </h1>
-          <p className="hero__description">
+          <p className="hero-text__description">
             Find a variety of properties that suit you very easily, forget all
             difficulties in finding a residence for you
           </p>
 
-          <form action="" className="hero__search">
+          <form action="" className="hero-search">
             <i className="bx bxs-map"></i>
             <input
               type="search"
               placeholder="Search by location..."
-              className="hero__search-input"
+              className="hero-search__input"
             />
             <button className="button">Search</button>
           </form>
 
-          <div className="hero__value">
+          <div className="hero-text__value">
             <div>
-              <h1 className="hero__value-number">
+              <h1 className="hero-text__value__number">
                 9K <span>+</span>
               </h1>
-              <span className="hero__value-description">
+              <span className="hero-text__value__description">
                 Premium <br /> Product
               </span>
             </div>
             <div>
-              <h1 className="hero__value-number">
+              <h1 className="hero-text__value__number">
                 2K <span>+</span>
               </h1>
-              <span className="hero__value-description">
+              <span className="hero-text__value__description">
                 Happy <br /> Customers
               </span>
             </div>
             <div>
-              <h1 className="hero__value-number">
+              <h1 className="hero-text__value__number">
                 28K <span>+</span>
               </h1>
-              <span className="hero__value-description">
+              <span className="hero-text__value__description">
                 Awards <br /> Won
               </span>
             </div>
           </div>
         </div>
-        <div className="hero__images">
-          <div className="home__orbe"></div>
-          <div className="home__img">
+        <div className="hero-images">
+          <div className="hero-images__orbe"></div>
+          <div className="hero-images__img">
             <img src="./src/assets/images/home.jpg" alt="" />
           </div>
         </div>
