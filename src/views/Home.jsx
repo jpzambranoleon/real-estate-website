438ff3423ff1d0b82fbfd395979fbd8446ba704d
Jean-Paul <jpzambrano.leon@gmail.com>
2023-07-18 18:33:07 -0500

    modified file

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import Logos from "../components/Logos";
 
 export default function Home() {
   return (
     <main className="main home">
       <Hero />
+      <Logos />
     </main>
   );
 }
