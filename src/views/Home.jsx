b8f4d1d0782294b8d464377e720e13c79b100d50
Jean-Paul <jpzambrano.leon@gmail.com>
2023-07-19 18:07:46 -0500

    modified file

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero";
 import Logos from "../components/Logos";
 import Popular from "../components/Popular";
+import Value from "../components/Value";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Logos />
       <Popular />
+      <Value />
     </main>
   );
 }
