cc6ad49dd0439de3d39ea7de2f2be768faadddc6
Jean-Paul <jpzambrano.leon@gmail.com>
2023-07-19 20:00:52 -0500

    created Subscribe.jsx

---
+++ src/components/Subscribe.jsx
@@ -0,0 +1,16 @@
+export default function Subscribe() {
+  return (
+    <section className="subscribe-section section" id="subscribe">
+      <div className="subscribe-container container">
+        <h1 className="subscribe-container__tile">Get Started with Holux</h1>
+        <p className="subscribe-container__description">
+          Subscribe and find super attractive price quotes from us, Find your
+          residence soon
+        </p>
+        <a href="#" className="button subscribe-container__button">
+          Get Started
+        </a>
+      </div>
+    </section>
+  );
+}
