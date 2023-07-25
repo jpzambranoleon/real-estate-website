e1cd4fb3a40c43ee928086113cf4b50b89618d4d
Jean-Paul <jpzambrano.leon@gmail.com>
2023-07-25 13:29:53 -0500

    modified file

--- src/components/ScrollUp.jsx
+++ src/components/ScrollUp.jsx
@@ -1,4 +1,14 @@
 export default function ScrollUp() {
+  function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up");
+
+    // When the scroll is higher than 350 viewport height, and the show-scroll class to the a tag with the scroll-top
+    if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-sroll");
+  }
+
+  window.addEventListener("scroll", scrollUp);
+
   return (
     <a href="#" className="scrollup" id="scroll-up">
       <i className="bx bx-chevrons-up"></i>
